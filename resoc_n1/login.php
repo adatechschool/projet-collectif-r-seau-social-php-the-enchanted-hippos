07b55c48c0fb992449c4bd2abbae90f0bc8549f4
Λυδяэφ ♫ <gueguen.audrey@yahoo.fr>
2024-02-13 10:29:18 +0100

    ajout lien loginSucceed

--- resoc_n1/login.php
+++ resoc_n1/login.php
@@ -90,6 +90,7 @@ session_start();?>
                             // Etape 7 : Se souvenir que l'utilisateur s'est connecté pour la suite
                             // documentation: https://www.php.net/manual/fr/session.examples.basic.php
                             $_SESSION['connected_id']=$user['id'];
+                            header('Location: loginSucceed.php');
                         }
                     }
                     ?>                     
